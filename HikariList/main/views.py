2b3cd4a50e08c61d85303ef21a36893554b94a21
Sergio Dorantes Godino <sdorgod3108@fp.iesromerovargas.com>
2025-05-04 23:09:00 +0200

    Update views.py

--- HikariList/main/views.py
+++ HikariList/main/views.py
@@ -1,35 +1,39 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Anime, Review
+from .forms import AnimeForm, ReviewForm
 
-from animes.models import Anime, Studio, Genre
+class AnimeListView(ListView):
+    model = Anime
+    template_name = 'anime/list.html'
+    paginate_by = 20
+    context_object_name = 'animes'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        genre = self.request.GET.get('genre')
+        if genre:
+            queryset = queryset.filter(genres__name=genre)
+        return queryset.order_by('-created_at')
 
-# Create your views here.
-@login_required
-def index(request):
-    animes = Anime.objects.all()
-    context = {'animes' : animes}
-    return render(request, 'animes/index.html', context)
+class AnimeDetailView(DetailView):
+    model = Anime
+    template_name = 'anime/detail.html'
 
-@login_required
-def anime_details(request, anime_id):
-    anime = Anime.objects.get(pk=anime_id)
-    context = {'anime' : anime}
-    return render(request, 'animes/anime_details.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['review_form'] = ReviewForm()
+        return context
 
-@login_required
-def studio_list(request):
-    studios = Studio.objects.all()
-    context = {'studios' : studios}
-    return render(request, 'animes/studio_list.html', context)
-@login_required
-def studio_details(request, studio_id):
-    studio = Studio.objects.get(pk=studio_id)
-    context = {'studio' : studio}
-    return render(request, 'animes/studio_details.html', context)
+class AnimeCreateView(LoginRequiredMixin, CreateView):
+    model = Anime
+    form_class = AnimeForm
+    template_name = 'anime/form.html'
 
-@login_required
-def genre_list(request):
-    genres = Genre.objects.all()
-    context = {'genres' : genres}
-    return render(request, 'animes/genre_list.html', context)
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('anime-detail', kwargs={'pk': self.object.pk})

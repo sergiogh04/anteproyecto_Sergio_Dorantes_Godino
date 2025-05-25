6c7132e678a0a4e267ce5d51e93af773e647db31
Sergio Dorantes Godino <sdorgod3108@fp.iesromerovargas.com>
2025-05-25 20:02:16 +0200

    Delete gestion_animes directory

--- gestion_animes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AnimeListView, AnimeDetailView, AddReviewView, UpdateListStatus
-
-urlpatterns = [
-    path('trending/', AnimeListView.as_view(), name='anime_list'),
-    path('anime/<int:pk>/', AnimeDetailView.as_view(), name='anime_detail'),
-    path('anime/<int:pk>/review/', AddReviewView.as_view(), name='add_review'),
-    path('anime/<int:pk>/update-status/', UpdateListStatus.as_view(), name='update_status'),
-]
